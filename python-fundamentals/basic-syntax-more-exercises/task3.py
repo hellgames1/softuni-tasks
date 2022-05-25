0523455c3e0c8f01905b6bfdeaace27e97e80f32
hellgames1 <hellygamesy@gmail.com>
2022-05-25 22:40:54 +0300

    solved task 3

---
+++ python-fundamentals/basic-syntax-more-exercises/task3.py
@@ -0,0 +1,10 @@
+""""
+Task 3 - Wolf in Sheep's clothing
+"""
+anims = input().split(", ")
+if anims[-1] == "wolf":
+    print("Please go away and stop eating my sheep")
+else:
+    for i in range(1,len(anims)+1):
+        if anims[-i]=="wolf":
+            print(f"Oi! Sheep number {i-1}! You are about to be eaten by a wolf!")
