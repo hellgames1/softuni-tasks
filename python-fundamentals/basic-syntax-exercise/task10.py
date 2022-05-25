366234211b2d7074e277412d2dfefd4a6f05b701
hellgames1 <hellygamesy@gmail.com>
2022-05-25 21:32:21 +0300

    realized I can't put the entire task description in each script, so I opted for just the title instead

--- python-fundamentals/basic-syntax-exercise/task10.py
+++ python-fundamentals/basic-syntax-exercise/task10.py
@@ -1,8 +1,5 @@
 """"
-You will be given two strings.
-Transform the first string into the second one, letter by letter, starting from the first one.
-After each interaction, print the resulting string only if it is unique.
-Note: the strings will have the same length
+Task 10 - Mutate Strings
 """
 
 first = input()
