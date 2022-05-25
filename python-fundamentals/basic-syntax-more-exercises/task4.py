410d15df1410433d3c35e3fabbc9db57fca8bef1
hellgames1 <hellygamesy@gmail.com>
2022-05-25 22:45:42 +0300

    solved task 4

---
+++ python-fundamentals/basic-syntax-more-exercises/task4.py
@@ -0,0 +1,6 @@
+""""
+Task 4 - Sum of a Beach
+"""
+stringg = input().lower()
+beach_words = stringg.count("sand") + stringg.count("water") + stringg.count("fish") + stringg.count("sun")
+print(beach_words)
