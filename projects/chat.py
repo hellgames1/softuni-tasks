b94053a9dcc465f5a4131d02f306ced14e97db5b
hellgames1 <hellygamesy@gmail.com>
2022-05-30 22:43:12 +0300

    added extra code for executable version (its commented out)

--- projects/chat.py
+++ projects/chat.py
@@ -1,9 +1,10 @@
 import requests
-import sys
+import sys#, os
 
 server_ip = "46.35.186.220"
 has_profile = requests.get(f"http://{server_ip}/index.php")
 errormsg = ""
+
 
 if has_profile.text == "false":
     print("Нов IP адрес. Нямате профил.\nМоля напишете потребителско име\n(само цифри и букви на латиница, до 30 на брой, без долни черти, разстояния и други символи)")
@@ -12,15 +13,20 @@ if has_profile.text == "false":
         chat = requests.get(f"http://{server_ip}/index.php?register=" + nick)
         if chat.text == "okay":
             print("Регистрацията беше успешна! Моля изпълнете скрипта наново, за да влезете в чата")
+            #input()
         else:
             print("Имаше грешка от страна на сървъра. Моля докладвайте на админа!\n(или пък сте барникали в python кода ;)")
+            #input()
     else:
         print("Името не спазва поставените ограничения! Моля изпълнете скрипта отново и опитайте пак!")
+        #input()
 else:
+    #_ = os.system('cls')
     usrname = requests.get(f"http://{server_ip}/{has_profile.text}.txt")
     print(f"Добре дошъл, {usrname.text}!\nИмай впредвид, че чатът не поддържа кирилица и съобщения над 160 символа!\nНатисни Enter, за да продължиш...")
     input()
     while True:
+        _ = os.system('cls')
         chat = requests.get(f"http://{server_ip}/chat.txt")
         chat_printable = chat.text.replace("@@","\n")
         print(chat_printable)
