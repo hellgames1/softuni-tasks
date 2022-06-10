5da5b300dc578c002f8956c9f4baf95fb6df3291
hellgames1 <hellygamesy@gmail.com>
2022-06-10 18:30:46 +0300

    solved task 2 - Prime Number Checker

---
+++ python-fundamentals/data-types-and-variables-more-exercises/task2.py
@@ -0,0 +1,10 @@
+""""
+Task 2 - Prime Number Checker
+"""
+num = int(input())
+is_prime = True
+for i in range(2,num):
+    if num % i == 0:
+        is_prime = False
+        break
+print(is_prime)
