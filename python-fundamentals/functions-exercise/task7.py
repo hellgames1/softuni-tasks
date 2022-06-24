6ffbfe097dc202a08d24f4a402e12078b36f8891
hellgames1 <hellygamesy@gmail.com>
2022-06-24 14:30:53 +0300

    solved task 7 - min max sum

---
+++ python-fundamentals/functions-exercise/task7.py
@@ -0,0 +1,5 @@
+""""
+Task 7 - Min Max Sum
+"""
+nums = [int(a) for a in input().split(" ")]
+print(f"The minimum number is {min(nums)}\nThe maximum number is {max(nums)}\nThe sum number is: {sum(nums)}")
