c0d5c89a4bb07e68861c72a9a416159f98a27cb0
hellgames1 <hellygamesy@gmail.com>
2022-06-24 15:21:05 +0300

    solved task 12 - factorial division

--- python-fundamentals/functions-exercise/task3.py
+++ python-fundamentals/functions-exercise/task3.py
@@ -5,3 +5,6 @@ char1 = ord(input())
 char2 = ord(input())
 result = lambda x,y: [chr(i) for i in range(x+1,y)]
 print(*result(char1,char2))
+
+# variant s edin red / one line variant
+print(*[chr(i) for i in range(ord(input())+1,ord(input()))])
